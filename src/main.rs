cb7846d00d3743e193746627be3d04fbee9cc10a
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-06-19 18:42:02 +0530

    add_employee fn added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{
+    collections::HashMap,
+    // io::{stdin, stdout},
+};
+
+type Company = HashMap<String, Vec<String>>;
+
 fn main() {
-    println!("Hello, world!");
+    let mut company: Company = HashMap::new();
+    println!("{:#?}", company);
+    // let mut input = String::new();
+
+    add_employee(&mut company, "Rohan".to_string(), "Engineering".to_string());
+}
+
+fn add_employee(company: &mut Company, employee: String, department: String) {
+    let dept = company
+        .entry(department.to_lowercase())
+        .or_insert(Vec::new());
+
+    dbg!(&dept);
+    dept.push(employee.to_lowercase());
+
+    dbg!(&dept);
+    dbg!(&company);
 }
