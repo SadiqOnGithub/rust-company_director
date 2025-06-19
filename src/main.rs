6181117d0e4287de41b1f9b569eea5818903bccc
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-06-19 18:43:48 +0530

    clean up

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ type Company = HashMap<String, Vec<String>>;
 
 fn main() {
     let mut company: Company = HashMap::new();
-    println!("{:#?}", company);
-    // let mut input = String::new();
 
     add_employee(&mut company, "Rohan".to_string(), "Engineering".to_string());
 }
@@ -18,9 +16,5 @@ fn add_employee(company: &mut Company, employee: String, department: String) {
         .entry(department.to_lowercase())
         .or_insert(Vec::new());
 
-    dbg!(&dept);
     dept.push(employee.to_lowercase());
-
-    dbg!(&dept);
-    dbg!(&company);
 }
