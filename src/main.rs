76bec98dfb1b52e4732e06902101febd9d20d915
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-06-20 06:44:25 +0530

    adding list_department fn

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,11 @@ type Company = HashMap<String, Vec<String>>;
 fn main() {
     let mut company: Company = HashMap::new();
 
-    add_employee(&mut company, "Rohan".to_string(), "Engineering".to_string());
+    add_employee(&mut company, "rohan".to_string(), "Engineering".to_string());
+    add_employee(&mut company, "mohan".to_string(), "Engineering".to_string());
+    add_employee(&mut company, "ohan".to_string(), "Engineering".to_string());
+    add_employee(&mut company, "sohan".to_string(), "Engineering".to_string());
+    list_department(&company, "Engineerin".to_string());
 }
 
 fn add_employee(company: &mut Company, employee: String, department: String) {
@@ -17,4 +21,15 @@ fn add_employee(company: &mut Company, employee: String, department: String) {
         .or_insert(Vec::new());
 
     dept.push(employee.to_lowercase());
+}
+
+fn list_department(company: &Company, department: String) {
+    match company.get(&department.to_lowercase()) {
+        Some(dept) => {
+            for employee in dept {
+                println!("{}", employee)
+            }
+        }
+        None => println!("department {} not exist", department),
+    }
 }
